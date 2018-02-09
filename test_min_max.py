f072e2983ed5d50f76277a6141f4403dee296951
David <jdl62@duke.edu>
2018-02-09 13:01:03 -0500

    changed test

--- test_min_max.py
+++ test_min_max.py
@@ -4,8 +4,8 @@ def test_min_max():
 
     min_max_val1 = min_max([1, 2, 3, 4, 5]) # normal list
     min_max_val2 = min_max([-1, -3, -5, 4, 2, 0]) # with negatives, out of order
-    min_max_val3 = min_max([-6, -5, -3, -1, 1, 1, 3])
+    min_max_val3 = min_max([-6.2, -5, -3, -1, 1.4, 1.4, 3]) # with repated non-integers
 
     assert(min_max_val1 == (1,5))
     assert(min_max_val2 == (-5,4))
-    assert(min_max_val3 == (-6,3))
+    assert(min_max_val3 == (-6.2,3))
